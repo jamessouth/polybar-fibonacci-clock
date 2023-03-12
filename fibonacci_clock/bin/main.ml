c6282bb0243b782dd9d851bfb5a9c956f64c9818
jamessouth <jamesdanielsouth@gmail.com>
2023-03-12 00:43:31 -0600

    format

--- fibonacci_clock/bin/main.ml
+++ fibonacci_clock/bin/main.ml
@@ -1,1 +1,9 @@
-let () = print_endline "Hello, World!"
+let time = Unix.localtime (Unix.time ())
+let rawhour = time.tm_hour
+let min = time.tm_min
+let sec = time.tm_sec
+
+let hour rh =
+  match rh < 13 with
+  | true -> ( match rh with 0 -> 12 | _ -> rh)
+  | false -> rh - 12
