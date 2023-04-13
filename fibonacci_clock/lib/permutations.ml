0fb041146eb3df19d3acd4d41add82cb638b9eb4
jamessouth <jamesdanielsouth@gmail.com>
2023-04-13 02:08:42 -0500

    perms

--- fibonacci_clock/lib/permutations.ml
+++ fibonacci_clock/lib/permutations.ml
@@ -12,13 +12,20 @@ let pl f lst =
   print_newline ()
 
 let pint = pl print_int
+let rec sum = function [] -> 0 | h :: t -> h + sum t
+
+(* let rec perm n res = function
+   | [] -> []
+   | h :: t ->
+         let ores = res in let res = h :: res in let z = sum res in
+         if z < n then perm n res t
+         else if z > n then perm n (List.tl (List.rev ores)) (h::t)
+          else res *)
 
 let rec perm n res = function
   | [] -> []
   | h :: t ->
       let y = n - h in
       if y < 0 then perm n res t
-      else if y > 0 then
-        let res = h :: res in
-        perm y res t
-      else List.rev (h :: res)
+      else if y > 0 then perm y (h :: res) t
+      else (h :: res)
