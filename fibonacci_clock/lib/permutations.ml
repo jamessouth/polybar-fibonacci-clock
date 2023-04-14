ab2bc341bb1441b1c6021cb35b1efd221fd2d202
jamessouth <jamesdanielsouth@gmail.com>
2023-04-14 01:41:39 -0500

    random perms

--- fibonacci_clock/lib/permutations.ml
+++ fibonacci_clock/lib/permutations.ml
@@ -14,13 +14,21 @@ let pl f lst =
 let pint = pl print_int
 let rec sum = function [] -> 0 | h :: t -> h + sum t
 
-(* let rec perm n res = function
-   | [] -> []
-   | h :: t ->
-         let ores = res in let res = h :: res in let z = sum res in
-         if z < n then perm n res t
-         else if z > n then perm n (List.tl (List.rev ores)) (h::t)
-          else res *)
+let get_rando_seq n l =
+  let rec inner n res ll =
+    match ll with
+    | [] ->
+        if sum res < n then failwith "n cannot be reached with these numbers"
+        else res
+    | _ ->
+        let r = Random.int (List.length ll) in
+        let res = List.nth ll r :: res in
+        let y = sum res in
+        if y = n then res
+        else if y > n then inner n [] l
+        else inner n res (List.filteri (fun i _x -> i != r) ll)
+  in
+  inner n [] l
 
 let rec perm n res = function
   | [] -> []
@@ -28,4 +36,4 @@ let rec perm n res = function
       let y = n - h in
       if y < 0 then perm n res t
       else if y > 0 then perm y (h :: res) t
-      else (h :: res)
+      else h :: res
