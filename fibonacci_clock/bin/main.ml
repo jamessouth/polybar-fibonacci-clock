b281081103009dc5e77e6b41761f9bda8cc4455d
jamessouth <jamesdanielsouth@gmail.com>
2023-05-24 02:03:54 -0500

    add acc mode

--- fibonacci_clock/bin/main.ml
+++ fibonacci_clock/bin/main.ml
@@ -47,6 +47,8 @@ let () =
        (let%map_open.Command args = args
         and seqs =
           flag "-seq" (one_or_more_as_pair sequences) ~doc:"string sequences"
+        and modes =
+          flag "-mode" (one_or_more_as_pair accuracy_modes) ~doc:"string accuracy modes"
         and gaps =
           flag "-gap" (one_or_more_as_pair int) ~doc:"int gap in pixels"
         and spaces =
@@ -59,18 +61,21 @@ let () =
           let open Fibonacci_clock.Time in
           if
             List.count args ~f:(fun x -> String.( = ) x "-seq")
-            <> List.count args ~f:(fun x -> String.( = ) x "-gap")
-          then failwith "number of sequences and gaps entered must be equal"
+            <> List.count args ~f:(fun x -> String.( = ) x "-gap") || List.count args ~f:(fun x -> String.( = ) x "-gap")
+            <> List.count args ~f:(fun x -> String.( = ) x "-mode")
+          then failwith "number of sequences, modes, and gaps entered must be equal"
           else
             let seq, acc = fst seqs in
             let seq1, acc1 = List.hd_exn (snd seqs) in
+            let acc_mode = fst modes in
+            let acc_mode1 = List.hd_exn (snd modes) in
             let gap = fst gaps in
             let gap1 = List.hd_exn (snd gaps) in
 
             if gap < 0 || gap1 < 0 then
               failwith "gaps must be unsigned integers"
             else
-              let first = { seq; gap; acc; colors } in
+              let first = { seq; gap; acc; acc_mode; colors } in
 
               match
                 ( List.count args ~f:(fun x -> String.( = ) x "-seq"),
@@ -88,6 +93,7 @@ let () =
                         seq = seq1;
                         gap = gap1;
                         acc = acc1;
+                        acc_mode = acc_mode1;
                         colors = sec_colors;
                       } )
                   |> main
